4833d810daa7032633f4973ee35c4195819397f0
vnayks <meetvince@gmail.com>
2018-06-19 11:14:05 -0500

    Update mergeSort-test.js

--- tests/mergeSort-test.js
+++ tests/mergeSort-test.js
@@ -3,49 +3,49 @@ var mergeSort = require('../src/sort/mergeSort.js')
 var utils = require('../tests/utils.js')
 
 describe('MergeSort', function() {
-  // describe('BasicTests', function() {
-  //   it('Should return true', function() {
-  //     var nums=[3,1,5,4];
-  //     var result=mergeSort.sort(nums);
-  //     assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('BasicTests', function() {
+    it('Should return true', function() {
+      var nums=[3,1,5,4];
+      var result=mergeSort.sort(nums);
+      assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('longArrayTest', function() {
-  //   it('Should return true', function() {
-  //     this.timeout(20000)
-  //     var nums=[];
-  //     for(var i=0;i<100000;i++){
-  //       nums.push(utils.randomIntFromInterval(0,100))
-  //     }            
-  //     var result=mergeSort.sort(nums);            
-  //     assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('longArrayTest', function() {
+    it('Should return true', function() {
+      this.timeout(20000)
+      var nums=[];
+      for(var i=0;i<100000;i++){
+        nums.push(utils.randomIntFromInterval(0,100))
+      }            
+      var result=mergeSort.sort(nums);            
+      assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('Negative integer input', function() {
-  //   it('Should return true', function() {
-  //     var nums=[-2,3,23,0,-99,-22,-12,-22222,-09,-234,-231,-929,-92];
-  //     var result=mergeSort.sort(nums);
-  //     assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('Negative integer input', function() {
+    it('Should return true', function() {
+      var nums=[-2,3,23,0,-99,-22,-12,-22222,-09,-234,-231,-929,-92];
+      var result=mergeSort.sort(nums);
+      assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('empty input', function() {
-  //   it('Should return true', function() {
-  //     var nums=[];
-  //     var result=mergeSort.sort(nums);
-  //     assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('empty input', function() {
+    it('Should return true', function() {
+      var nums=[];
+      var result=mergeSort.sort(nums);
+      assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('Input all same elements', function() {
-  //   it('Should return true', function() {
-  //     var nums=[1,1,1,1,1,1,1,1,1,1,1,1];
-  //     var result=mergeSort.sort(nums);
-  //     assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('Input all same elements', function() {
+    it('Should return true', function() {
+      var nums=[1,1,1,1,1,1,1,1,1,1,1,1];
+      var result=mergeSort.sort(nums);
+      assert.isTrue(utils.checkIfSorted(result), "sorted array is :" + result );      
+    });
+  });
 
 });
 
@@ -59,40 +59,40 @@ describe('MergeSort Descending Tests', function() {
     });
   });
 
-  // describe('longArrayTest', function() {
-  //   it('Should return true', function() {
-  //     this.timeout(20000)
-  //     var nums=[];
-  //     for(var i=0;i<10;i++){
-  //       nums.push(utils.randomIntFromInterval(0,100000))
-  //     }
-  //     var result=mergeSort.sortDescending(nums);            
-  //     assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('longArrayTest', function() {
+    it('Should return true', function() {
+      this.timeout(20000)
+      var nums=[];
+      for(var i=0;i<10;i++){
+        nums.push(utils.randomIntFromInterval(0,100000))
+      }
+      var result=mergeSort.sortDescending(nums);            
+      assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('Negative integer input', function() {
-  //   it('Should return true', function() {
-  //     var nums=[-2,3,23,0,-99,-22,-12,-22222,-09,-234,-231,-929,-92];
-  //     var result=mergeSort.sortDescending(nums);
-  //     assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('Negative integer input', function() {
+    it('Should return true', function() {
+      var nums=[-2,3,23,0,-99,-22,-12,-22222,-09,-234,-231,-929,-92];
+      var result=mergeSort.sortDescending(nums);
+      assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('empty input', function() {
-  //   it('Should return true', function() {
-  //     var nums=[];
-  //     var result=mergeSort.sortDescending(nums);
-  //     assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('empty input', function() {
+    it('Should return true', function() {
+      var nums=[];
+      var result=mergeSort.sortDescending(nums);
+      assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
+    });
+  });
 
-  // describe('Input all same elements', function() {
-  //   it('Should return true', function() {
-  //     var nums=[1,1,1,1,1,1,1,1,1,1,1,1];
-  //     var result=mergeSort.sortDescending(nums);
-  //     assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
-  //   });
-  // });
+  describe('Input all same elements', function() {
+    it('Should return true', function() {
+      var nums=[1,1,1,1,1,1,1,1,1,1,1,1];
+      var result=mergeSort.sortDescending(nums);
+      assert.isTrue(utils.checkIfSorted(result,true), "sorted array is :" + result );      
+    });
+  });
 
 });
